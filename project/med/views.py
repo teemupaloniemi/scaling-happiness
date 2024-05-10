4449c3543aeb23abf7b51d466cc7e85b288ff3a8
tealjapa <tealjapa@student.jyu.fi>
2024-05-10 16:08:30 +0300

    patch 2: logging visible only to admin

--- project/med/views.py
+++ project/med/views.py
@@ -2,17 +2,21 @@ import time
 from django.shortcuts import render, redirect
 from django.forms.models import model_to_dict
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 from med.models import Records
+
 
 def add(request):
     if request.method == "POST":
         Records.objects.create(user=request.user, body=request.POST.get('body'))
     return redirect('/')
 
+
 def clear(request):
     User.objects.all().delete()
     return redirect('/')
+
 
 def getData(request):
     if request.user and request.user.is_authenticated:
@@ -27,12 +31,14 @@ def getData(request):
     with open('log.txt', 'a', encoding='utf-8') as log_file:
         log_file.write(f"==== {time.strftime('%m/%d/%Y', time.localtime(time.time()))} - { time.strftime('%H:%M:%S', time.localtime(time.time())) } | Unauthenticated user tried to requested data ====\n") 
     return render(request, 'med/index.html')
-    
+
+
 def home_view(request):
     if request.user and request.user.is_authenticated: 
         records = request.session.get('records', [])
         return render(request, 'med/data.html', {'records': records}) 
     return render(request, 'med/index.html')
+
 
 def login(request):
     if request.method == "POST":
@@ -50,6 +56,7 @@ def login(request):
     log_file.write(f"==== {time.strftime('%m/%d/%Y', time.localtime(time.time()))} - { time.strftime('%H:%M:%S', time.localtime(time.time())) } | Login attemp with wrong method ====\n")
     return redirect('/')
 
+
 def logout(request):
     if request.user and request.user.is_authenticated:
         with open('log.txt', 'a', encoding='utf-8') as log_file:
@@ -61,13 +68,20 @@ def logout(request):
     return redirect('/')
 
 
+# pathc 2: helper for log tests
+def isadmin(user):
+        return user.is_authenticated and user.is_staff
+
+
+@login_required # patch 2: this way only authenticated users can see the logs
+@user_passes_test(isadmin, login_url='/') # patch 2: only admin is allowed to see the logs
 def log(request):
     log_data = []
     with open('log.txt', 'r', encoding='utf-8') as log_file:
         for line in log_file:
             log_data.append(line)
-    print(log_data)
     return render(request, 'med/log.html', {'data': log_data})
+
 
 def create_users(request):
     User.objects.create_user(username='bob', password='squarepants')
@@ -76,7 +90,5 @@ def create_users(request):
     print("created alice redqueen")
     User.objects.create_user(username='test', password='test')
     print("created test test")
-    User.objects.create_user(username='admin', password='secret')
-    print("created admin secret")
  
     return redirect('/')
